d73594d14b663152d7d70dbd93795045e34fa4e4
supreeth <supreethratam@gmail.com>
2022-06-07 14:04:21 +0530

    Increased Roll Number Font size and added association

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -7,12 +7,14 @@ export default function Invoice(props) {
   let year = `${today.getFullYear()}`;
   return (
     <div className="invoice">
-      <div className="header d-flex justify-content-between mb-6">
+      <div className="header d-flex justify-content-start mb-6">
         <div ><img src="https://telegra.ph/file/c52384c7dbffa6bb5d162.png" alt="not found" className="logo"/>
-        <h2>Fund for 2021 Batch</h2></div>
-        <h3  id="main-head">Mechanical Engineering</h3>
-        <h1 className="pad">Invoice</h1>
+        <h2>Fund for 2021 Batch</h2>
+        </div>
+        <h3  id="main-head">Mechanical Engineering Association</h3>
+        
       </div>
+      <h1 className="pad">Invoice</h1>
       <div className="details my-3">
         
         <div className="name">
